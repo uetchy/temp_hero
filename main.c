06ad2e39e3338a4dd077b87e5c68ab09f4f7a097
Ndreaz <slyferanthem@gmail.com>
2014-11-14 16:12:12 +0900

    added some main.c part

--- main.c
+++ main.c
@@ -58,7 +58,7 @@ typedef struct
     int y                 // 現在地Y
 } Player
 
-areaList = [
+Room areaList = [
     Room area1[8][8],
     Room area2[8][8]
 ]
@@ -74,3 +74,71 @@ const int Right		= 1;
 const int Down		= 2;
 const int Left		= 3;
 
+// 関数のプロトタイプ
+void initialisearealist();
+void initialisePlayer();
+void initialiseGameEndingConditions();
+void printGameExplanation();
+void showarealist();
+void showRoomInfo();
+int getUserMove();
+void movePlayer( int );
+void checkEncountGauge( void );
+void tryReadHint( void );
+void tryDrinkPotion( void );
+void tryTakeKey( void );
+
+int main( void )
+{
+	char c;
+	int moveDirection;				// 移動方向
+
+	// 初期化
+	initialisearealist();					// ダンジョンの初期化
+	initialisePlayer();						// プリンスの情報を初期化
+	initialiseGameEndingConditions();		// ゲームの終了条件を初期化
+	printGameExplanation();					// ゲームのルールを表示
+
+	// ゲームループ：勝利条件と敗北条件が満たしていないまで続く
+	while( 1 )
+	{
+		showarealist();							// ダンジョンの情報を表示
+		showRoomInfo();							// 部屋の情報を表示
+
+		checkEncountGauge();					// Stinkerがいるなら戦闘
+		if( PlayerDead )						// Stinkerに負けた（敗北）
+		{
+			break;
+		}
+		if( rescuedPrincess )					// Super Stinkerを倒した（勝利）
+		{
+			break;
+		}
+		tryDrinkPotion();						// ポーションがあるか、使うかのチェック
+		tryReadHint();							// ヒントあるかどうか
+		tryTakeKey								// 鍵があるかどうか
+		moveDirection = getUserMove();			// ユーザーから移動方向を入力してもらう
+		movePlayer( moveDirection );			// ユーザが選んだ方向にプリンスを移動させる
+
+	}
+
+	// 勝利か敗北のメッセージでゲーム終了する
+	printf( "*************\n" );
+	printf( "* GAME OVER *\n" );
+	printf( "*************\n\n" );
+	if( rescuedPrincess )
+	{
+		printf( "Boss: You might have defeated me, but it's not the end.\n" );
+		printf( "You have defeated this dungeon boss and you managed to escape from this dungeon.\n" );
+		printf( "But the Hero's tale is just begin.\n" );
+	}
+	else
+	{
+		printf( "You died in battle and your body is eaten by the monsters.\n" );
+		printf( "Of course, the world is destroyed.\n" );
+	}
+	printf( "\nPress any key to continue.\n" );
+	c = getchar();
+
+	return 0;
+}

2769461085857012d61e860f6f64a4442856b7af
Ndreaz <terra_costa@hotmail.com>
2014-11-14 15:46:51 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 typedef struct
 {
     int uniqueBossId,  // 固有敵のインデックス（nullならいない）
@@ -21,3 +23,15 @@ areaList = [
     Room area1[8][8],
     Room area2[8][8]
 ]
+
+// 道の状況
+const int noPath		= 0;
+const int openPath		= 1;
+const int lockedDoor	= 2;
+
+// 方向
+const int Up		= 0;
+const int Right		= 1;
+const int Down		= 2;
+const int Left		= 3;
+

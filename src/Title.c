b660858a6513ddc209ffda38f1e6a1f7ba88cf59
YRAchelY <c0112532a5@edu.teu.ac.jp>
2014-11-28 16:06:00 +0900

    Title

---
+++ src/Title.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+
+  printf("ll 　 ll 　      　　 lll　 　 　 　 　 　 　              \n");
+  printf("ll,,,,ll　ll''''''ll,　ll ,,l''' ,,ll'''''ll,　 l,''''''ll,\n" );
+  printf("ll 　 ll ,,lll'''''lll lll''ll,,　ll''''''''''' lll 　  ll\n" );
+  printf("ll 　 ll 'll,,,,,l'lll lll　''ll, 'll,,,,,,l''  lll 　  ll \n"); 
+   printf("\n\n");
+
+  printf("'ll,, ,ll''\n");
+  printf("''ll,ll''　,ll 　   ll lll   　 ll    ,ll''''''ll,   ll, ,ll'   ll''''''ll,\n");
+  printf("   ll　　   ll 　   ll lll 　   ll　   ''''llll,,　   ll,,ll'  ,,,ll'''''lll\n");
+  printf("   ll' 　　 ll,,,,,l'l　ll,,,,,'ll'  'll,,,,,,l''　  'lll'　    'll,,,,,,'lll\n");
+  printf("                     　　                        　,,,ll'\n");
+
+    return 0;
+
+}

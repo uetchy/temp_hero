da057127dd61ea907404cbf39c932fa3bae90636
Ndreaz <slyferanthem@gmail.com>
2014-12-03 09:34:37 +0900

    ルール
    タイトルの下にルールやストーリを追加した。
    注．英文

--- src/image.c
+++ src/image.c
@@ -12,4 +12,28 @@ void printTitle() {
   printf("   ll　　   ll 　   ll lll 　   ll　   ''''llll,,　   ll,,ll'  ,,,ll'''''lll\n");
   printf("   ll' 　　 ll,,,,,l'l　ll,,,,,'ll'  'll,,,,,,l''　  'lll'　    'll,,,,,,'lll\n");
   printf("                     　　                        　,,,ll'\n");
-}
+
+  printf( "\nRules of Hakken Yuusha:\n");
+		printf( "==========================\n" );
+		printf( "1) You are a hero that was summoned inside of a dungeon\n" );
+		printf( "2) You need to get out from the dungeon to save the world\n" );
+		printf( "3) But since the path lead to the  outside is blocked by rocks\n" );
+		printf( "4) You need to get a shovels that the boss use as a weapon\n" );
+		printf( "5) There are more than 1 boss\n" );
+		printf( "6) Defeating the hidden boss will improve your chances of winning\n" );
+		printf( "7) In the fight, you can attack different part of the enemies and one of them is their weak point\n" );
+		printf( "8) Each enemies have different weak point\n" );
+		printf( "9) Attacking the weak point will deal bigger damage to the enemies\n" );
+		printf( "10) There are tons of hint inside the dungeon left by some other adventurers\n" );
+		printf( "11) Some of the hint are useful, but some are not .\n" );
+		printf( "12) You can heal yourself by using potion found in the dungeon\n" );
+		printf( "13) The potion will heal you back to full health when used outside of battle\n" );
+		printf( "14) Or heal for 10 HP when used in battle\n" );
+		printf( "15) Using potion when battling will use your turn, so you can't use potion and attack at the same time\n" );
+		printf( "14) Or heal for 10 HP when used in battle\n" );
+		printf( "\nPress any key to start the game.\n" );
+	
+	}
+	printf( "Good luck with your quest to escape the dungeon\n" );
+	}
+  }

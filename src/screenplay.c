0570dfedf8ad41b01c0d7b658d4b76f9e2f86b0c
Yasuaki Uechi <uetchy@randompaper.co>
2014-12-06 02:17:07 +0900

    タイトル表示をマシに

--- src/screenplay.c
+++ src/screenplay.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 void printTitle() {
-  printf("|   ll 　 ll 　      　　 lll　 　 　 　 　 　 　              \n");
-  printf("|   ll,,,,ll　ll''''''ll,　ll ,,l''' ,,ll'''''ll,　 l,''''''ll,\n" );
-  printf("|   ll 　 ll ,,lll'''''lll lll''ll,,　ll''''''''''' lll 　  ll\n" );
-  printf("|   ll 　 ll 'll,,,,,l'lll lll　''ll, 'll,,,,,,l''  lll 　  ll \n");
-  printf("|   \n|   \n");
-  printf("|   'll,, ,ll''\n");
-  printf("|   ''ll,ll''　,ll 　   ll lll   　 ll    ,ll''''''ll,   ll, ,ll'   ll''''''ll, \n");
-  printf("|      ll　　   ll 　   ll lll 　   ll　   ''''llll,,　   ll,,ll'  ,,,ll'''''lll \n");
-  printf("|      ll' 　　 ll,,,,,l'l　ll,,,,,'ll'  'll,,,,,,l''　  'lll'　    'll,,,,,,'lll\n");
-  printf("|                        　　                        　,,,ll'\n");
+  printf("\n\n\n\n\n\n\n");
+  printf("                    ll 　 ll 　      　　 lll　 　 　 　 　 　 　              \n");
+  printf("                    ll,,,,ll　ll''''''ll,　ll ,,l''' ,,ll'''''ll,　 l,''''''ll,\n" );
+  printf("                    ll 　 ll ,,lll'''''lll lll''ll,,　ll''''''''''' lll 　  ll\n" );
+  printf("                    ll 　 ll 'll,,,,,l'lll lll　''ll, 'll,,,,,,l''  lll 　  ll \n");
+  printf("                    \n");
+  printf("                    \n");
+  printf("                    'll,, ,ll''\n");
+  printf("                    ''ll,ll''　,ll 　   ll lll   　 ll    ,ll''''''ll,   ll, ,ll'   ll''''''ll, \n");
+  printf("                       ll　　   ll 　   ll lll 　   ll　   ''''llll,,　   ll,,ll'  ,,,ll'''''lll \n");
+  printf("                       ll' 　　 ll,,,,,l'l　ll,,,,,'ll'  'll,,,,,,l''　  'lll'　    'll,,,,,,'lll\n");
+  printf("                                         　　                        　,,,ll'\n");
 }
 
 void printRulesEN() {

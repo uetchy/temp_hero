9db1575bb722b59eb8297cea24adade831f53cb0
Ndreaz <terra_costa@hotmail.com>
2014-12-05 17:11:18 +0900

    blablablablabla

--- src/screenplay.c
+++ src/screenplay.c
@@ -27,10 +27,12 @@ void printRulesEN() {
   printf( "9) Attacking the weak point will deal bigger damage to the enemies\n" );
   printf( "10) There are tons of hint inside the dungeon left by some other adventurers\n" );
   printf( "11) Some of the hint are useful, but some are not .\n" );
-  printf( "12) You can heal yourself by using potion found in the dungeon\n" );
-  printf( "13) The potion will heal you back to full health when used outside of battle\n" );
-  printf( "14) Or heal for 10 HP when used in battle\n" );
-  printf( "15) Using potion when battling will use your turn, so you can't use potion and attack at the same time\n" );
+  printf( "12) Every time you move or read a hint, the encount gauge will fill up\n" );
+  printf( "13) After the encount gauge is filled to a certain point, you will be force to fight\n" );
+  printf( "14) You can heal yourself by using potion found in the dungeon\n" );
+  printf( "15) The potion will heal you back to full health when used outside of battle\n" );
+  printf( "16) Or heal for 10 HP when used in battle\n" );
+  printf( "17) Using potion when battling will use your turn, so you can't use potion and attack at the same time\n" );
 
   printf( "Good luck with your quest to escape the dungeon\n" );
 }
@@ -38,8 +40,24 @@ void printRulesEN() {
 void printRulesJP() {
   printf( "\n派遣勇者のルール:\n");
   printf( "==========================\n" );
-  printf( "1) まだ完成していません\n" );
-  printf( "2) 英語の方を見てください\n" );
+  printf( "01) プレイヤーはダンジョンの中に召喚された勇者です\n" );
+  printf( "02) プレイヤーはこのダンジョンから脱出し、世界を救わなければいけません\n" );
+  printf( "03) しかしこのダンジョンの出口は岩で塞がれています\n" );
+  printf( "04) その岩を壊すために、ダンジョンボースに武器として使われているシャベルが必要です\n" );
+  printf( "05)　このダンジョンではボス１つ以上います\n" );
+  printf( "06) その裏ボスを倒すとプレイヤーはパワーアップし、ダンジョンから脱出可能性がもっと広がる\n" );
+  printf( "07) 敵（雑魚とボス）は複数の部位を持ち、その中に１つの弱点部位がいます\n" );
+  printf( "08) 敵の種類による、弱点部位は変わります\n" );
+  printf( "09) 弱点部位に攻撃すると、敵に大ダメージを与えることができます\n" );
+  printf( "10) ダンジョン内には以前冒険した冒険者のヒントがたくさんあります\n" );
+  printf( "11) そのヒントは大事なヒントだけではなく、意味不明のヒントもあります\n" );
+  printf( "12) プレイヤーは動くたびにとヒントを見るときエンカウントゲージが１つたまります\n" );
+  printf( "13) エンカウントゲージが溜まると、強制に敵と戦うことになります\n" );
+  printf( "12) ダンジョン内に見つかったポションはマップ中に使うと全回することになりますが、\n" );
+  printf( "13) 戦闘中に使うと、HPが１０しか回復しません\n" );
+  printf( "14) 戦闘中にポションを使うと自分のターンが使うため、そのターンに敵を攻撃することができません\n" )
+  printf( "X) まだ完成していません\n" );
+  printf( "X) もっと詳しいルールは英語の方に書かれてあります\n" );
   
   printf( "では。。。健闘を祈る\n" );
 }
